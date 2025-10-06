354d5d16ec0b2c3cff9e2e0f022b42e22262e462
danilo.barbosa <danilobarbosa@ecoa.puc-rio.br>
2025-10-06 19:30:51 -0300

    chore: projeto Maven (Model + JUnit4 + loaders XLSX/JSON/YAML)

---
+++ src/main/java/banco_imobiliario_ID/banco_imobiliario/App.java
@@ -0,0 +1,10 @@
+package banco_imobiliario_ID.banco_imobiliario;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/banco_imobiliario_ID/banco_imobiliario/AppTest.java
@@ -0,0 +1,19 @@
+package banco_imobiliario_ID.banco_imobiliario;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
