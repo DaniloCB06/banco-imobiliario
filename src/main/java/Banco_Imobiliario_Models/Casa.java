38b93d0f9996d3597c763242cae4ba31d802f45e
Gabriel da Silva <129470082+GabSMF@users.noreply.github.com>
2025-10-11 14:08:00 -0300

    Estrutura básica de Casa

--- src/main/java/Banco_Imobiliario_Models/Casa.java
+++ src/main/java/Banco_Imobiliario_Models/Casa.java
@@ -1,5 +1,19 @@
 package Banco_Imobiliario_Models;
 
 public class Casa {
-
+	protected int posicao;
+	protected String nome;
+	protected String tipo;
+	
+	
+	public Casa(int posicao, String nome, String tipo) {
+        this.posicao = posicao;
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+	
+	
+	public int getPosicao() { return posicao; }
+    public String getNome() { return nome; }
+    public String getTipo() { return tipo; }
 }
