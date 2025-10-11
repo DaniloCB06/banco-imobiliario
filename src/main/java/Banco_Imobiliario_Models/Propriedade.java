241051c71ba8d205724682b55931043e13a0eaac
Gabriel da Silva <129470082+GabSMF@users.noreply.github.com>
2025-10-11 14:07:39 -0300

    Dados de Propriedade

--- src/main/java/Banco_Imobiliario_Models/Propriedade.java
+++ src/main/java/Banco_Imobiliario_Models/Propriedade.java
@@ -1,5 +1,28 @@
 package Banco_Imobiliario_Models;
 
-public class Propriedade {
+import java.util.Map;
+import java.util.HashMap;
 
+public class Propriedade extends Casa {
+    protected int precoCompraCasa;
+    protected int precoCompraHotel;
+    private Map<Integer, Integer> alugueis;
+
+    public Propriedade(String nome, int precoCasa, int precoHotel, int[] valoresAluguel) {
+        super(0, nome, "Propriedade");
+        this.precoCompraCasa = precoCasa;
+        this.precoCompraHotel = precoHotel;
+
+        // Usa HashMap mutável
+        this.alugueis = new HashMap<>();
+
+        // Preenche os valores de aluguel
+        for (int i = 0; i < valoresAluguel.length; i++) {
+            alugueis.put(i, valoresAluguel[i]);
+        }x
+    }
+
+    public int getAluguel(int numCasas) {
+        return alugueis.getOrDefault(numCasas, 0); // 0 sem casa ou 5 se for hotel
+    }
 }
